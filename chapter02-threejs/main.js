f99230e6986c91d1f3da003f3e9b61b8108ad20d
seldy21 <seldy@naver.com>
2024-02-04 12:40:08 +0900

    extrude, 구 mesh 예제

--- chapter02-threejs/main.js
+++ chapter02-threejs/main.js
@@ -16,7 +16,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.y = 1;
-camera.position.z = 5;
+camera.position.z = 6;
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
 directionalLight.castShadow = true;
@@ -47,7 +47,80 @@ capsuleMesh.position.set(3, 1.75, 0);
 capsuleMesh.castShadow = true;
 capsuleMesh.receiveShadow = true;
 
-scene.add(capsuleMesh)
+scene.add(capsuleMesh);
+
+const cylinderGeometry = new THREE.CylinderGeometry(1, 1, 2);
+const cylinderMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+cylinderMesh.position.set(-3, 1, 0);
+cylinderMesh.castShadow = true;
+cylinderMesh.receiveShadow = true;
+
+scene.add(cylinderMesh);
+
+const torusGeometry = new THREE.TorusGeometry(0.5, 0.1, 16.1, 100);
+const torusMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+torusMesh.position.set(0, 0.5, 1);
+torusMesh.castShadow = true;
+torusMesh.receiveShadow = true;
+
+scene.add(torusMesh);
+
+const starShape = new THREE.Shape();
+starShape.moveTo(0, 1);
+starShape.lineTo(0.2, 0.2);
+starShape.lineTo(1, 0.2);
+starShape.lineTo(0.4, -0.1);
+starShape.lineTo(0.6, -1);
+starShape.lineTo(0, -0.5);
+starShape.lineTo(-0.6, -1);
+starShape.lineTo(-0.4, -0.1);
+starShape.lineTo(-1, 0.2);
+starShape.lineTo(-0.2, 0.2);
+
+const shapeGeometry = new THREE.ShapeGeometry(starShape);
+const shapeMaterial = new THREE.MeshStandardMaterial({ color: 0xff00ff });
+const shapeMesh = new THREE.Mesh(shapeGeometry, shapeMaterial);
+shapeMesh.position.set(0, 1, 2);
+
+scene.add(shapeMesh);
+
+const extrudeSettings = {
+  steps: 1,
+  depth: 0.1,
+  //모서리를 둥글게 하는 값
+  bevelEnabled: true,
+  //bevel의 두께
+  bevelThickness: 0.3,
+  bevelSize: 0.3,
+  //bavel된 모서리를 매끄럽게 하는 값
+  bevelSegments: 100,
+};
+
+const extrudeGeomerty = new THREE.ExtrudeGeometry(starShape, extrudeSettings);
+const extrudeMaterial = new THREE.MeshStandardMaterial({ color: 0x0ddaaf });
+const extrudeMesh = new THREE.Mesh(extrudeGeomerty, extrudeMaterial);
+extrudeMesh.position.set(2, 1.3, 2);
+extrudeMesh.castShadow = true;
+extrudeMesh.receiveShadow = true;
+
+scene.add(extrudeMesh);
+
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x98daaf });
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh.position.set(0, 1, -3);
+scene.add(sphereMesh);
+
+const numPoints = 1000;
+const positions = new Float32Array(numPoints * 3);
+
+for (let i = 0; i < numPoints; i++) {
+  const x = (Math.random() - 0.5) * 1;
+  const y = (Math.random() - 0.5) * 1;
+  const z = (Math.random() - 0.5) * 1;
+}
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.update();
